9e7da2244bd7ce7daba1d0bb29804615df643946
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2019-02-10 18:01:07 -0200

    Atualização dos códigos, e alteração do código inicial de aula.

--- src/com/rodrigofujioka/javabasico/aula01/PrimeiraAula.java
+++ src/com/rodrigofujioka/javabasico/aula01/PrimeiraAula.java
@@ -15,36 +15,29 @@
 package com.rodrigofujioka.javabasico.aula01;
 
 /**
- * Primeiro programa java.
- * Classe de apresentação utilizada para entender
- * o ambiente a compilação de programas com Java.
+ * Primeiro programa java. Classe de apresentação utilizada para entender o
+ * ambiente a compilação de programas com Java.
  * 
  * @author Rodrigo.fujioka
- * @data 03/02/2016  21:30 
+ * @data 03/02/2016 21:30 2018.1 
+ * @date 07/02/2019 20:30 2019.1
  *
  */
 public class PrimeiraAula {
-	
+
 	/*
-	 * main método principal de uma classe executavel java.
-	 * O compilador java procura o main método em uma classe para executar
-	 * se ela não tem esse método ela não é executavel
+	 * main método principal de uma classe executavel java. O compilador java
+	 * procura o main método em uma classe para executar, se ela não tem esse método
+	 * ela não é executavel
 	 */
-	public static void main(String[] args) {
-		
-		//syso Imprime texto no console
-		System.out.println("Eu vou ser Fera em Java");
-		
-		String nome = "Aluno de Rodrigo Fujioka";
-		
-		nome = "Fome";
-		
-		nome = "Sono";
-		
-		 
-		
+	public static void main(String args[]) {
+		imprimeNome();
+		System.out.println(args[0]);
+		System.out.println(args[1]);
 	}
-	
-	
+
+	public static void imprimeNome() {
+		System.out.println("Alô mundo");
+	}
 
 }
