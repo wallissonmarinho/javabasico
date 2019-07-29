8cbbc2bf7679b2c0222ed788761fb6ef3914aea6
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2019-07-28 22:26:34 -0300

    Ajustando Arquivos para aulas de 2019.2, incluindo prática no código

--- src/com/rodrigofujioka/javabasico/aula00/HelloWorld.java
+++ src/com/rodrigofujioka/javabasico/aula00/HelloWorld.java
@@ -1,25 +1,37 @@
 /**
- * Projeto das trilhas de treinamento de Java básico ou avançado 
- * com foco nas certificações java e em treinamentos corporativos. 
+ * Projeto das trilhas de treinamento de Java básico ou avançado
+ * com foco nas certificações java e em treinamentos corporativos.
  * Fontes disponíveis em https://github.com/rodrigofujioka
- * 
+ * <p>
  * Professor: Rodrigo da Cruz Fujioka
  * Ano: 2019
  * http://www.rodrigofujioka.com
  * http://www.fujideia.com.br
  * http://lattes.cnpq.br/0843668802633139
- * 
- * Contato: rcf4@cin.ufpe.br 
- * 
+ * <p>
+ * Contato: rcf4@cin.ufpe.br
  */
 package com.rodrigofujioka.javabasico.aula00;
 
 /**
- * Classe que define o comportamento para o objeto.
+ * Primeira classe da turma, nessa aula deve ser explorado
+ * a estrutura básica do Java, diferença entre arquivos .java e .class
+ * comandos java e javac além da diferença entre as implementações Java
+ * OpenJDK , OracleJDK e outras implementações.
+ *
  * @author: Rodrigo Fujioka
- * @date: 4 de fev de 2019 
- * @Time: 02:20:21
+ * @date: 28 de Julho de 2019
+ * @Time: 20:20:21
  */
 public class HelloWorld {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Primeira APP Java ->  Turma 2019.2");
+        //crt + space
+        //crt + shift + F ->  eclipse
+        //crt + alt + L  -> Intellij Idea
+    }
 
 }
--- src/com/rodrigofujioka/javabasico/aula01/PrimeiraAula.java
+++ src/com/rodrigofujioka/javabasico/aula01/PrimeiraAula.java
@@ -1,43 +1,48 @@
 /**
- * Projeto das trilhas de treinamento de Java básico ou avançado 
- * com foco nas certificações java e em treinamentos corporativos. 
+ * Projeto das trilhas de treinamento de Java básico ou avançado
+ * com foco nas certificações java e em treinamentos corporativos.
  * Fontes disponíveis em https://github.com/rodrigofujioka
- * 
+ * <p>
  * Professor: Rodrigo da Cruz Fujioka
- * 
+ * <p>
  * http://www.rodrigofujioka.com
  * http://www.fujideia.com.br
  * http://lattes.cnpq.br/0843668802633139
- * 
- * Contato: rcf4@cin.ufpe.br 
- * 
+ * <p>
+ * Contato: rcf4@cin.ufpe.br
  */
 package com.rodrigofujioka.javabasico.aula01;
 
 /**
- * Primeiro programa java. Classe de apresentação utilizada para entender o
- * ambiente a compilação de programas com Java.
- * 
- * @author Rodrigo.fujioka
- * @data 03/02/2016 21:30 2018.1 
- * @date 07/02/2019 20:30 2019.1
+ * Primeiro programa java. Classe de apresentação utilizada para entender e reforçar
+ * as informações da primeira Aula, Passagem de parâmetros e métodos em Java.
+ * Prática:
+ *   1 - Implemente um programa que recebe parâmetros com o nome e ano de nascimento.
+ *   o programa deve calcular a idade do usuário em anos com base no ano de nascimento informado.
  *
+ *   2 - Implemente uma calculadora em Java, utilizando métodos e o que você achar que é possível
+ *   com seu conhecimento atual, para inteiros use o tipo   int.
+ *
+ * @author Rodrigo.fujioka
+ * @data 03/02/2016 21:30 2018.1
+ * @date 07/02/2019 20:30 2019.1
  */
 public class PrimeiraAula {
 
-	/*
-	 * main método principal de uma classe executavel java. O compilador java
-	 * procura o main método em uma classe para executar, se ela não tem esse método
-	 * ela não é executavel
-	 */
-	public static void main(String args[]) {
-		imprimeNome();
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-	}
+    /*
+     * main método principal de uma classe executavel java. O compilador java
+     * procura o main método em uma classe para executar, se ela não tem esse método
+     * ela não é executavel
+     */
+    public static void main(String args[]) {
 
-	public static void imprimeNome() {
-		System.out.println("Alô mundo");
-	}
+        imprimeNome();
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+
+    public static void imprimeNome() {
+        System.out.println("Alô mundo");
+    }
 
 }
