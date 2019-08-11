487a903e85ba5773d4c4c60d03cb2d830e5831f7
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2019-08-11 19:23:29 -0300

    Realizando ajustes no código, os códigos até o final da unidade 1 vão possuir comentários, depois vão seguir os princípios do clean code

--- src/com/rodrigofujioka/javabasico/aula03/Aluno.java
+++ src/com/rodrigofujioka/javabasico/aula03/Aluno.java
@@ -1,16 +1,15 @@
 /**
- * Projeto das trilhas de treinamento de Java básico ou avançado 
- * com foco nas certificações java e em treinamentos corporativos. 
+ * Projeto das trilhas de treinamento de Java básico ou avançado
+ * com foco nas certificações java e em treinamentos corporativos.
  * Fontes disponíveis em https://github.com/rodrigofujioka
- * 
+ * <p>
  * Professor: Rodrigo da Cruz Fujioka
  * Ano: 2016
  * http://www.rodrigofujioka.com
  * http://www.fujideia.com.br
  * http://lattes.cnpq.br/0843668802633139
- * 
- * Contato: rcf4@cin.ufpe.br 
- * 
+ * <p>
+ * Contato: rcf4@cin.ufpe.br
  */
 package com.rodrigofujioka.javabasico.aula03;
 
@@ -21,22 +20,34 @@ package com.rodrigofujioka.javabasico.aula03;
  * @Time: 21:25:42
  */
 public class Aluno {
-	
-	String nome;
-	int idade;
-	String escola;
 
-	public void exibeDados(){		
-		System.out.println("Nome " + nome);
-		System.out.println("Idade " + idade);
-		System.out.println("Escola " + escola);	
-	}	
-	
-	public void informaIdade(){
-		System.out.println("Idade " + idade);
-	}
-	
-	
+    String nome;
+    int idade;
+    String escola;
+
+    /**
+     * Método que exibe todos os dados do usuário.
+     */
+    public void exibeDados() {
+        System.out.println("Nome " + nome);
+        System.out.println("Idade " + idade);
+        System.out.println("Escola " + escola);
+    }
+
+
+    /**
+     * Método que exibe apenas a idade do usuário
+     */
+    public void informaIdade() {
+        System.out.println("Idade " + idade);
+    }
+
+    /*
+     * Crie métodos que sejam responsáveis pela impressão das demais variaveis que não possuem impressão solo
+     *
+     * */
+
+
 }
 
 
--- src/com/rodrigofujioka/projeto/calculadora/Calculadora.java
+++ src/com/rodrigofujioka/projeto/calculadora/Calculadora.java
@@ -1,14 +1,14 @@
 package com.rodrigofujioka.projeto.calculadora;
 
-public class Calculadora {
+public interface Calculadora {
 
-    public void somar(int numeroA, int numeroB){}
+    public void somar(int numeroA, int numeroB);
 
-    public void subtrair(int numeroA, int numeroB){}
+    public void subtrair(int numeroA, int numeroB);
 
-    public void multiplicar(int numeroA, int numeroB){}
+    public void multiplicar(int numeroA, int numeroB);
 
-    public void dividir(int numeroA, int numeroB){}
+    public void dividir(int numeroA, int numeroB);
 
 
 }
